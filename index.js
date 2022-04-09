1dd3e6c8f8938b53c254183d03cc10d819dac248
obraia <bryan22czo@gmail.com>
2022-04-09 10:25:46 -0300

    Refatoração para lançamento da biblioteca npm

--- index.js
+++ index.js
@@ -1,65 +1,62 @@
-import { IntervaloConfianca } from "./intervalo_confianca.js";
-import { TabelaProbabilidade } from "./tabela.js";
+import { IntervaloConfianca } from "./lib/intervalo_confianca.js";
+import { TabelaProbabilidades } from "./lib/tabela_probabilidades.js";
 
 /**
- * Se o intervalo contém zero, então as soluções não são diferentes, ou seja, precisa de mais testes para diferenciá-las.
- * Se o intervalo é positivo, neste exemplo, a segunda solução é a melhor.
- * Se o intervalo é negativo, neste exemplo, a primeira solução é a melhor.
+ * Se o intervalo da diferença contém zero, então as soluções não são diferentes, ou seja, precisa de mais testes para diferenciá-las.
+ * Se o intervalo da diferença é positivo, neste exemplo, a segunda solução é a melhor.
+ * Se o intervalo da diferença é negativo, neste exemplo, a primeira solução é a melhor.
+ *
+ * @param {Array<number>} amostra_a
+ * @param {Array<number>} amostra_b
+ * @param {'0.600'| '0.700'| '0.800'| '0.900'| '0.950'| '0.975'| '0.990'| '0.995'} nivel_confianca
+ *
+ * @returns {{
+ *  intervalo_confianca_a: Array<number>;
+ *  desvio_padrao_a: number;
+ *  media_a: number;
+ *  intervalo_confianca_b: Array<number>;
+ *  desvio_padrao_b: number;
+ *  media_b: number;
+ *  intervalo_confianca_diferenca: Array<number>;
+ *  desvio_padrao_diferenca: number;
+ *  media_diferenca: number;
+ * }}
  */
-function main() {
-  const amostra_a = [25, 27, 33, 32, 25, 26, 31, 28];
-  const amostra_b = [28, 21, 19, 20, 26, 28, 19, 20];
-
-  const tabela = new TabelaProbabilidade("./dados/tabela-probabilidade.csv");
-
-  // 0.600,0.700,0.800,0.900,0.950,0.975,0.990,0.995
-  const nivel_confianca = "0.900";
+function main(amostra_a, amostra_b, nivel_confianca) {
+  const tabela = new TabelaProbabilidades("./dados/tabela-probabilidade.csv");
   const valor_tabela = tabela.obter_valor(amostra_a.length, nivel_confianca);
 
-  const { 
-    intervalo_confianca: intervalo_confianca_a ,
-  } = calcular_intervalo(
-    amostra_a,
-    valor_tabela
-  );
-  const { 
-    intervalo_confianca: intervalo_confianca_b 
-  } = calcular_intervalo(
-    amostra_b,
-    valor_tabela
-  );
+  const {
+    intervalo_confianca: intervalo_confianca_a,
+    desvio_padrao: desvio_padrao_a,
+    media: media_a,
+  } = IntervaloConfianca.calcular_intervalo(amostra_a, valor_tabela);
+
+  const {
+    intervalo_confianca: intervalo_confianca_b,
+    desvio_padrao: desvio_padrao_b,
+    media: media_b,
+  } = IntervaloConfianca.calcular_intervalo(amostra_b, valor_tabela);
 
   const diferenca = IntervaloConfianca.diferenca(amostra_a, amostra_b);
 
-  const { 
-    intervalo_confianca: intervalo_confianca_diferenca 
-  } = calcular_intervalo(
-    diferenca, 
-    valor_tabela
-  );
+  const {
+    intervalo_confianca: intervalo_confianca_diferenca,
+    desvio_padrao: desvio_padrao_diferenca,
+    media: media_diferenca,
+  } = IntervaloConfianca.calcular_intervalo(diferenca, valor_tabela);
 
-  console.table({
+  return {
     intervalo_confianca_a,
+    desvio_padrao_a,
+    media_a,
     intervalo_confianca_b,
+    desvio_padrao_b,
+    media_b,
     intervalo_confianca_diferenca,
-    nivel_confianca,
-  });
+    desvio_padrao_diferenca,
+    media_diferenca,
+  };
 }
 
-function calcular_intervalo(amostra = [], valor_tabela = 0) {
-  const media = IntervaloConfianca.media(amostra);
-
-  const desvio_padrao = IntervaloConfianca.desvio_padrao(amostra, media);
-
-  const intervalo_confianca = IntervaloConfianca.calcular_confianca(
-    media,
-    valor_tabela,
-    desvio_padrao,
-    amostra.length
-  );
-
-  return { intervalo_confianca, media, desvio_padrao };
-}
-
-console.clear();
-main();
+export default main;
